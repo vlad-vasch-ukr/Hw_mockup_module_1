987c7eb46e42fd3806a68e6e215203189f0daf47
Vladislav <vasch_ukr@ukr.net>
2021-07-20 21:24:13 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+  $('#slider').slick({
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: false,
+    draggable: true,
+    infinite: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    speed: 1000,
+    swipe: true
+  })
+})
